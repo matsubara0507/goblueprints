a0c1ed0a2cf5258f009ffc12e2ba008545109255
松原信忠 <t12307043@gunma-u.ac.jp>
2017-01-16 22:44:52 +0900

    Updated to code in chap. 1.1.1

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte(`
-      <html>
-        <head>
-          <title>チャット</title>
-        </head>
-        <body>
-          チャットしましょう！
-        </body>
-      </html>
-    `))
-  })
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    log.Fatal("ListenAndServe:", err)
-  }
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
