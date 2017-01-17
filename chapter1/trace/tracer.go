fa3122761696a0f3d669fe15e8db1d4f310799e8
松原信忠 <t12307043@gunma-u.ac.jp>
2017-01-17 23:39:32 +0900

    Updated to trace in apendix B

--- chapter1/trace/tracer.go
+++ chapter1/trace/tracer.go
@@ -5,27 +5,17 @@ import (
 	"io"
 )
 
-type Tracer interface {
-	Trace(...interface{})
-}
-
-type tracer struct {
+type Tracer struct {
 	out io.Writer
 }
 
-func (t *tracer) Trace(a ...interface{}) {
-	t.out.Write([]byte(fmt.Sprint(a...)))
-	t.out.Write([]byte("\n"))
+func (t Tracer) Trace(a ...interface{}) {
+	if t.out == nil {
+		return
+	}
+	fmt.Fprintln(t.out, a...)
 }
 
 func New(w io.Writer) Tracer {
-	return &tracer{out: w}
-}
-
-type nilTracer struct{}
-
-func (t *nilTracer) Trace(a ...interface{}) {}
-
-func Off() Tracer {
-	return &nilTracer{}
+	return Tracer{out: w}
 }
