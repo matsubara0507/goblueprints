3b84c56e251d74c2dda9e59f9661a9c0d6d39fca
松原信忠 <t12307043@gunma-u.ac.jp>
2017-01-18 13:53:09 +0900

    Created auth Handler
    but, not created auth page yet

---
+++ chapter2/chat/auth.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("auth")
+	if err == http.ErrNoCookie {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
