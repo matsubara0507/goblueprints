f0b021e22856713591ba617153f24eec298fdaec
松原信忠 <t12307043@gunma-u.ac.jp>
2017-01-18 13:31:47 +0900

    copy chapter1/chat
    fix trace path

---
+++ chapter2/chat/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"../../chapter1/trace"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	err := t.templ.Execute(w, r)
+	if err != nil {
+		log.Fatal("ServeHTTP: ", err)
+	}
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
+	log.Println("Webサーバーを開始します。ポート: ", *addr)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
