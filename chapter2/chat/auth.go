572e95162346432e263ff243002dd5181e2ffa2c
松原信忠 <t12307043@gunma-u.ac.jp>
2017-01-18 14:36:18 +0900

    Created login handler (chap. 2.3)
    but, do not process auth yet

--- chapter2/chat/auth.go
+++ chapter2/chat/auth.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type authHandler struct {
@@ -22,4 +25,19 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+// loginHandler handles the third-party login process.
+// format of path: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理 ", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション %s には非対応です", action)
+	}
 }
