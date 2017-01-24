611d796ebafa3f5621b3cd619e68a965b8584e7b
松原信忠 <t12307043@gunma-u.ac.jp>
2017-01-24 22:17:17 +0900

    Added logout

--- chapter3/chat/auth.go
+++ chapter3/chat/auth.go
@@ -15,8 +15,8 @@ type authHandler struct {
 }
 
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_, err := r.Cookie("auth")
-	if err == http.ErrNoCookie {
+	cookie, err := r.Cookie("auth")
+	if err == http.ErrNoCookie || cookie.Value == "" {
 		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	} else if err != nil {
