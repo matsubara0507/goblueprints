9f712dfae068467eed1ada3df73cd1ee8471ebfb
松原信忠 <t12307043@gunma-u.ac.jp>
2017-01-25 00:07:40 +0900

    Created avatar interface

---
+++ chapter3/chat/avatar.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+)
+
+// ErrNoAvatarURL is the error that is returned when the
+// Avatar instance is unable to provide an avatar URL.
+var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL")
+
+type Avatar interface {
+	GetAvatarURL(c *client) (string, error)
+}
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	url, ok := c.userData["avatar_url"]
+	if ok {
+		urlStr, ok := url.(string)
+		if ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
---
+++ chapter3/chat/avatar_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAuthAvatar(t *testing.T) {
+	var authAvatar AuthAvatar
+	client := new(client)
+	url, err := authAvatar.GetAvatarURL(client)
+	if err != ErrNoAvatarURL {
+		t.Error("値が存在しない場合、",
+			"AuthAvatar.GetAvatarURL は ErrNoAvatarURL を返すべきです")
+	}
+
+	testUrl := "http://url-to-avatar"
+	client.userData = map[string]interface{}{"avatar_url": testUrl}
+	url, err = authAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("値が存在する場合、",
+			"AuthAvatar.GetAvatarURL はエラーを返すべきではありません: ", err)
+	} else {
+		if url != testUrl {
+			t.Error("AuthAvatar.GetAvatarURL は正しいURLを返すべきです: ", url)
+		}
+	}
+}
