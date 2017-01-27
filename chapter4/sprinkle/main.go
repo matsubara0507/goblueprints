ee71948a4b29721789996e28641f0ab5f4a7d5f2
松原信忠 <t12307043@gunma-u.ac.jp>
2017-01-27 17:07:37 +0900

    Changed to use config yaml

--- chapter4/sprinkle/main.go
+++ chapter4/sprinkle/main.go
@@ -2,33 +2,43 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
 	"strings"
 	"time"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
-const otherWord = "*"
-
-var transforms = []string{
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord + "app",
-	otherWord + "site",
-	otherWord + "time",
-	"get" + otherWord,
-	"go" + otherWord,
-	"lets " + otherWord,
+type Config struct {
+	OtherWord  string   `yaml:"otherWord"`
+	Transforms []string `yaml:"transforms"`
 }
 
 func main() {
+	var filename = flag.String("config", "config.yaml", "変換方法")
+	flag.Parse()
+
+	buf, err := ioutil.ReadFile(*filename)
+	if err != nil {
+		log.Fatal("transforms file is not found.")
+	}
+
+	var config Config
+	err = yaml.Unmarshal(buf, &config)
+	if err != nil {
+		log.Fatal("error Unmarshal: ", buf)
+	}
+
 	rand.Seed(time.Now().UTC().UnixNano())
 	s := bufio.NewScanner(os.Stdin)
+
 	for s.Scan() {
-		t := transforms[rand.Intn(len(transforms))]
-		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+		t := config.Transforms[rand.Intn(len(config.Transforms))]
+		fmt.Println(strings.Replace(t, config.OtherWord, s.Text(), -1))
 	}
 }
